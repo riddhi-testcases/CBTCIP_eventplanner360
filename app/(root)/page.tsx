1f54a9ff2fdb4aa1a74ada2195406342a7e69886
Riddhi Chakraborty <riddhi.bbghs@gmail.com>
2024-06-27 23:58:45 +0530

    improved layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,34 +24,34 @@ export default async function Home({ searchParams }: SearchParamProps) {
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <h1 className="h1-bold">Unleash Your Event's Potential: Plan, Organize, Celebrate with Us!</h1>
+            <p className="p-regular-20 md:p-regular-24">Unlock exclusive insights and strategies from over 4000 seasoned experts across top-tier global corporations, all within our vibrant community.</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
-                Explore Now
+                Get Started
               </Link>
             </Button>
           </div>
 
-          <Image 
-            src="/assets/images/hero.png"
+          <Image
+            src="/assets/images/hero2.png"
             alt="hero"
-            width={1000}
-            height={1000}
+            width={4000}
+            height={2000}
             className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
           />
         </div>
-      </section> 
+      </section>
 
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+        <h2 className="h2-bold">Trust by <br /> Our Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
           <CategoryFilter />
         </div>
 
-        <Collection 
+        <Collection
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
