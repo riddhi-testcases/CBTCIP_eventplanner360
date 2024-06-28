2f4efade9649b9d34dab6056be4d5e294060fd8c
Riddhi Chakraborty <riddhi.bbghs@gmail.com>
2024-06-28 20:43:23 +0530

    Updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs/server";
- 
-export default authMiddleware({
-  publicRoutes: [
-    '/',
-    '/events/:id',
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ],
-  ignoredRoutes: [
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ]
-});
- 
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware(
+  
+);
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).)', '/', '/(api|trpc)(.)'],
+  matcher: ["/((?!.\\..|_next).)", "/", "/(api|trpc)(.)"],
 };
